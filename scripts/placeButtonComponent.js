e720e1dcaca195db39e9bac5f072b0333c6a3ce1
Michael Galo <michael@michaelthinks.co>
2024-06-25 09:27:36 -0500

    fixed duplication error

--- scripts/placeButtonComponent.js
+++ scripts/placeButtonComponent.js
@@ -1,11 +1,11 @@
 import { placeCarOrder } from "./transientState.js";
 
 export const orderButton = () => {
-  document.addEventListener("click", (clickEvent) => {
-    if (clickEvent.target.id === "orderButton") {
-      placeCarOrder();
-    }
-  });
-
   return `<button id="orderButton">Create Custom Order</button>`;
 };
+
+document.addEventListener("click", (clickEvent) => {
+  if (clickEvent.target.id === "orderButton") {
+    placeCarOrder();
+  }
+});
